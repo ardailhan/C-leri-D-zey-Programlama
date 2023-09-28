f5761da6a9e3cfa3eee65cc4cccc91b3a9e6ade5
ardailhan <ardailhan_@outlook.com>
2023-09-28 13:52:21 +0300

    Add project files.

---
+++ Dizilere_Derinlemesine_Bakis/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] d1 = (int[])Array.CreateInstance(typeof(int), 2);
+int[,,] d2 = (int[,,])Array.CreateInstance(typeof(int), 2, 3, 4);
+
+(string s, int i, bool b)[] d3 = new (string s, int i, bool b)[] {("asdasd", 123, true), ("asdasd213", 123, true), ("asda", 123, false) };
+
+Console.WriteLine();
