c4bddf0330aec08d54b5bca58193c6992fcec8a4
ardailhan <ardailhan_@outlook.com>
2023-09-28 14:33:16 +0300

    ref return and ref local

---
+++ ref_return_and_ref_local/Program.cs
@@ -0,0 +1,20 @@
+﻿//RefLocals
+char o = 'a';
+ref char p = ref o;
+
+
+//RefReturn
+int b = 5;
+int c = X(ref b);
+Console.WriteLine(b);
+Console.WriteLine(c);
+c = 123123123;
+Console.WriteLine(b);
+b = 123123;
+Console.WriteLine(c);
+
+ref int X(ref int a)
+{
+    a = 124;
+    return ref a;
+}
