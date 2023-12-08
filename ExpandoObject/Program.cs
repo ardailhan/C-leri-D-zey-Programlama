634a302775f954b2862cc69570ee7855157ce67a
ardailhan <ardailhan_@outlook.com>
2023-12-08 14:55:10 +0300

    ExpandoObject

---
+++ ExpandoObject/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Dynamic;
+
+dynamic person = new ExpandoObject();
+person.Name = "Arda";
+person.Surname = "İlhan";
+person.Age = "32";
+person.YearOfBirth = new Func<int>(() => DateTime.UtcNow.Year - person.Age);
