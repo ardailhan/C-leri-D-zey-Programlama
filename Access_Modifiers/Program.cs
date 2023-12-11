67ad056fca75a591febad431b626a582652b1107
ardailhan <ardailhan_@outlook.com>
2023-12-11 13:03:41 +0300

    Detailed Access Modifiers

---
+++ Access_Modifiers/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1;
+
+Class1 c = new();
+//X metodunun erişim belirleyicisi artık private oldugu için erişim kısıtlanmıstır.
+//c.X();
+c.Y();
+//Z metodu protected olarak olusturuldugu için instance üzerinden erişim sağlanamaz.
+//c.Z();
+
+class MyClass : Class1
+{
+    public MyClass()
+    {
+        //Internal olarak belirlenen bir metod ya da sınıf ne oluşturulduğu sınıfın instance'ı üzerinden ne de kalıtım yoluyla erişilemez durumdadır.
+        //W();
+        //Protected internal internal olmasına rağmen aynı zamanda Protected özelliği barındırdıgı için kalıtım alınan bir sınıf içinde oluşturulduysa bir başka proje üzerinden erişim sağlanabilir.
+        Q();
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,53 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private void X()
+        {
+
+        }
+        //Aynı sınıf içerisinde private olmasına ragmen X metoduna erişim sağlanabilir.
+        public void Y()
+        {
+            //Aynı class içinde tanımlandıkları için hem private hem protected olarak tanımlanmıs metodlara erişim sağlanabilmektedir.
+            X();
+            Z();
+        }
+        protected void Z()
+        {
+
+        }
+        internal void W()
+        {
+
+        }
+        protected internal void Q()
+        {
+
+        }
+        private protected void A()
+        {
+
+        }
+    }
+    public class Class2 : Class1
+    {
+        public Class2()
+        {
+            Class1 c = new();
+            //class2 sınıfı class1 sınıfından kalıtım aldıgından dolayı Z metodunun erişim belirteci Protected olmasına rağmen erişim sağlanabilmektedir.
+            Z();
+            //Aynı proje içerinden internal olarak oluşturulmus bir metoda erişim sağlayabilmekteyiz.
+            c.W();
+            //Q metodu protected internal belirtecinin internal özelliğinden dolayı instance üzerinden erişim sağlayabilmekteyiz
+            c.Q();
+            //Private protected olarak işaretlenmiş bir metoda Instance üzerinden erişim sağlayamayız ancak protected oldugu ve kalıtım söz konusu oldugu için A metoduna erişebiliyoruz.
+            A();
+        }
+    }
+    //Classlar default olarak internal halindedir.
+    class MyClass2
+    {
+
+    }
+}
